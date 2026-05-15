84c89e6d9d2f18220d51a5663c30cb4d16113ea6
양휘원 <127097595+yangone8249@users.noreply.github.com>
2026-05-15 14:54:28 +0900

    Delete src/services/supabase.ts

--- src/services/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://yrtgkudjdhteszorfxik.supabase.co';           // 여기에 Project URL 붙여넣기
-const SUPABASE_ANON_KEY = 'sb_publishable_BSnTVp8WmJBzDFYAH3-nZg_VUmmaRRI'; // 여기에 공개 가능한 API 키 붙여넣기
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
